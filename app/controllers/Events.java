5df6dd41590c63f72621b333a76cbcdcd16c4572
Jacques-Olivier D. Bernier <jdesjardinsbernier@gmail.com>
2013-09-16 17:36:29 -0400

    format code

--- app/controllers/Events.java
+++ app/controllers/Events.java
@@ -9,21 +9,20 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Events extends Controller {
-	
+
 	@Inject
 	private EventDao eventDao;
-	
-    public Result index() {
+
+	public Result index() {
 		return ok(Json.toJson(eventDao.list()));
-    }
-    
-    public Result show(long id) {
-    	Event event = eventDao.read(id);
-    	if(event == null){
-    		return notFound();
-    	}
-    	else{
-    		return ok(Json.toJson(event));
-    	}
-    }
+	}
+
+	public Result show(long id) {
+		Event event = eventDao.read(id);
+		if (event == null) {
+			return notFound();
+		} else {
+			return ok(Json.toJson(event));
+		}
+	}
 }
