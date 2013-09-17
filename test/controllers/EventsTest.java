d37605a3604b434cb1404904294f7683d5811926
Sylvain Perron <slvn.perron@gmail.com>
2013-09-16 21:46:34 -0400

    refactored test names

--- test/controllers/EventsTest.java
+++ test/controllers/EventsTest.java
@@ -10,7 +10,7 @@ import static play.test.Helpers.running;
 public class EventsTest extends BaseTest {
 
     @Test
-    public void index() {
+    public void indexReturnsAllEvents() {
         running(fakeApplication(global), new Runnable() {
             public void run() {
                 JsonNode jsonNode = getFakeJsonResponseFrom("/events");
@@ -19,38 +19,33 @@ public class EventsTest extends BaseTest {
 
                 JsonNode event1 = jsonNode.get(0);
                 assertThat(event1.get("id").asLong()).isEqualTo(1);
-                assertThat(event1.get("totalNumberOfTickets").asInt())
-                        .isEqualTo(1320);
+                assertThat(event1.get("totalNumberOfTickets").asInt()).isEqualTo(1320);
 
                 JsonNode event2 = jsonNode.get(1);
                 assertThat(event2.get("id").asLong()).isEqualTo(2);
-                assertThat(event2.get("totalNumberOfTickets").asInt())
-                        .isEqualTo(1320);
+                assertThat(event2.get("totalNumberOfTickets").asInt()).isEqualTo(1320);
             }
         });
     }
 
-
-
     @Test
-    public void show() {
+    public void showReturnsEvent() {
         running(fakeApplication(global), new Runnable() {
             public void run() {
                 JsonNode jsonNode = getFakeJsonResponseFrom("/events/1");
 
                 assertThat(jsonNode.get("id").asLong()).isEqualTo(1);
                 assertThat(jsonNode.get("categories").isArray());
+
                 JsonNode category1 = jsonNode.get("categories").get(0);
                 assertThat(category1.get("id").asLong()).isEqualTo(1);
                 assertThat(category1.get("price").asDouble()).isEqualTo(12.0);
-                assertThat(category1.get("numberOfTickets").asInt()).isEqualTo(
-                        120);
+                assertThat(category1.get("numberOfTickets").asInt()).isEqualTo(120);
 
                 JsonNode category2 = jsonNode.get("categories").get(1);
                 assertThat(category2.get("id").asLong()).isEqualTo(2);
                 assertThat(category2.get("price").asDouble()).isEqualTo(8.0);
-                assertThat(category2.get("numberOfTickets").asInt()).isEqualTo(
-                        1200);
+                assertThat(category2.get("numberOfTickets").asInt()).isEqualTo(1200);
             }
         });
     }
