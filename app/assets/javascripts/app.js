6621a8b25a9c39942059a621e146a6dfd7ae77f8
Francois-Xavier <frxpoulin@gmail.com>
2013-09-21 12:42:55 -0400

    remove javascript libs and add a page for single event

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,6 +6,8 @@ app.config(['$routeProvider', '$locationProvider', function(routeProvider, locat
     locationProvider.hashPrefix('!')
 
     routeProvider.when('/events', {controller: 'EventsController', templateUrl: '/assets/events.html'});
+    routeProvider.when('/events/:eventId', {controller: 'EventController', templateUrl: '/assets/event.html'});
 }]);
 
 app.controller('EventsController', ['$scope', '$http', controllers.events]);
+app.controller('EventController', ['$scope', '$http', '$routeParams', controllers.event]);
--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,8 +1,18 @@
 var events = function($scope, $http) {
     $http.get('/api/events').success(function(result){
-        console.log(result);
         $scope.events = result;
     });
 }
 
-module.exports = {events: events};
+var event = function($scope, $http, $routeParams) {
+    var eventId = $routeParams.eventId;
+    
+    $http.get('/api/events/' + eventId).success(function(result){
+        $scope.event = result
+    });
+}
+
+module.exports = {
+    events: events,
+    event: event
+};
