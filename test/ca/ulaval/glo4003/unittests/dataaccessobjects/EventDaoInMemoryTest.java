268e73f52efdde5522dbc7901d518282c8a1028d
Sylvain Perron <sylvain.perron@protorisk.com>
2013-09-21 14:51:37 -0400

    Ajout des AAA

--- test/ca/ulaval/glo4003/unittests/dataaccessobjects/EventDaoInMemoryTest.java
+++ test/ca/ulaval/glo4003/unittests/dataaccessobjects/EventDaoInMemoryTest.java
@@ -20,50 +20,63 @@ public class EventDaoInMemoryTest {
 
     @Test
     public void searchAllThenReturnsAllEvents() {
+        // Arrange
         Event firstEvent = EventsTestHelper.createRandomEventGivenSport(1, EventsTestHelper.FIRST_RANDOM_SPORT);
         Event secondEvent = EventsTestHelper.createRandomEventGivenSport(2, EventsTestHelper.SECOND_RANDOM_SPORT);
 
         eventDao.create(firstEvent);
         eventDao.create(secondEvent);
 
+        // Act
         List<Event> result = eventDao.search(new EventSearchCriteria());
+
+        // Assert
         Assertions.assertThat(result.size()).isEqualTo(2);
     }
 
     @Test
     public void searchSportThenReturnsFilteredResults() {
+        // Arrange
         Event firstEvent = EventsTestHelper.createRandomEventGivenSport(1, EventsTestHelper.FIRST_RANDOM_SPORT);
         Event secondEvent = EventsTestHelper.createRandomEventGivenSport(2, EventsTestHelper.SECOND_RANDOM_SPORT);
 
         eventDao.create(firstEvent);
         eventDao.create(secondEvent);
 
+        // Act
         EventSearchCriteria eventSearchCriteria = new EventSearchCriteria();
         eventSearchCriteria.setSportName(EventsTestHelper.SECOND_RANDOM_SPORT);
 
         List<Event> result = eventDao.search(eventSearchCriteria);
+
+        // Assert
         Assertions.assertThat(result.size()).isEqualTo(1);
         Assertions.assertThat(result.get(0).getSport().getName()).isEqualTo(EventsTestHelper.SECOND_RANDOM_SPORT);
     }
 
     @Test
     public void searchTeamThenReturnsFilteredResults() {
+        // Arrange
         Event firstEvent = EventsTestHelper.createRandomEventGivenTeam(1, EventsTestHelper.A_RANDOM_TEAM_NAME);
         Event secondEvent = EventsTestHelper.createRandomEventGivenTeam(2, EventsTestHelper.A_SECOND_RANDOM_TEAM_NAME);
 
         eventDao.create(firstEvent);
         eventDao.create(secondEvent);
 
+        // Act
         EventSearchCriteria eventSearchCriteria = new EventSearchCriteria();
         eventSearchCriteria.setTeamName(EventsTestHelper.A_RANDOM_TEAM_NAME);
 
         List<Event> result = eventDao.search(eventSearchCriteria);
+
+        // Assert
         Assertions.assertThat(result.size()).isEqualTo(1);
         Assertions.assertThat(result.get(0).getTeam().getName()).isEqualTo(EventsTestHelper.A_RANDOM_TEAM_NAME);
     }
 
     @Test
     public void searchDateStartWhenDateIsBeforeEventThenReturnsFilteredResults() {
+        // Arrange
         Event firstEvent = EventsTestHelper.createRandomEventGivenSport(1, EventsTestHelper.FIRST_RANDOM_SPORT);
         Event secondEvent = EventsTestHelper.createRandomEventGivenSport(2, EventsTestHelper.SECOND_RANDOM_SPORT);
 
@@ -77,16 +90,20 @@ public class EventDaoInMemoryTest {
         eventDao.create(firstEvent);
         eventDao.create(secondEvent);
 
+        // Act
         EventSearchCriteria eventSearchCriteria = new EventSearchCriteria();
         eventSearchCriteria.setDateStart(dayTwo);
 
         List<Event> result = eventDao.search(eventSearchCriteria);
+
+        // Assert
         Assertions.assertThat(result.size()).isEqualTo(1);
         Assertions.assertThat(result.get(0).getId()).isEqualTo(secondEvent.getId());
     }
 
     @Test
     public void searchDateStartWhenDateIsSameDayAsEventThenReturnsFilteredResults() {
+        // Arrange
         Event firstEvent = EventsTestHelper.createRandomEventGivenSport(1, EventsTestHelper.FIRST_RANDOM_SPORT);
         Event secondEvent = EventsTestHelper.createRandomEventGivenSport(2, EventsTestHelper.SECOND_RANDOM_SPORT);
 
@@ -99,16 +116,20 @@ public class EventDaoInMemoryTest {
         eventDao.create(firstEvent);
         eventDao.create(secondEvent);
 
+        // Act
         EventSearchCriteria eventSearchCriteria = new EventSearchCriteria();
         eventSearchCriteria.setDateStart(dayThree);
 
         List<Event> result = eventDao.search(eventSearchCriteria);
+
+        // Assert
         Assertions.assertThat(result.size()).isEqualTo(1);
         Assertions.assertThat(result.get(0).getId()).isEqualTo(secondEvent.getId());
     }
 
     @Test
     public void searchDateEndWhenDateIsAfterEventThenReturnsFilteredResults() {
+        // Arrange
         Event firstEvent = EventsTestHelper.createRandomEventGivenSport(1, EventsTestHelper.FIRST_RANDOM_SPORT);
         Event secondEvent = EventsTestHelper.createRandomEventGivenSport(2, EventsTestHelper.SECOND_RANDOM_SPORT);
 
@@ -122,16 +143,20 @@ public class EventDaoInMemoryTest {
         eventDao.create(firstEvent);
         eventDao.create(secondEvent);
 
+        // Act
         EventSearchCriteria eventSearchCriteria = new EventSearchCriteria();
         eventSearchCriteria.setDateEnd(veryFarAway);
 
         List<Event> result = eventDao.search(eventSearchCriteria);
+
+        // Assert
         Assertions.assertThat(result.size()).isEqualTo(1);
         Assertions.assertThat(result.get(0).getId()).isEqualTo(firstEvent.getId());
     }
 
     @Test
     public void searchDateEndWhenDateIsSameDayAsEventThenReturnsFilteredResults() {
+        // Arrange
         Event firstEvent = EventsTestHelper.createRandomEventGivenSport(1, EventsTestHelper.FIRST_RANDOM_SPORT);
         Event secondEvent = EventsTestHelper.createRandomEventGivenSport(2, EventsTestHelper.SECOND_RANDOM_SPORT);
 
@@ -144,16 +169,20 @@ public class EventDaoInMemoryTest {
         eventDao.create(firstEvent);
         eventDao.create(secondEvent);
 
+        // Act
         EventSearchCriteria eventSearchCriteria = new EventSearchCriteria();
         eventSearchCriteria.setDateEnd(dayOne);
 
         List<Event> result = eventDao.search(eventSearchCriteria);
+
+        // Assert
         Assertions.assertThat(result.size()).isEqualTo(1);
         Assertions.assertThat(result.get(0).getId()).isEqualTo(firstEvent.getId());
     }
 
     @Test
     public void searchDatesRangeThenReturnsFilteredResultsWithinDateRange() {
+        // Arrange
         Event firstEvent = EventsTestHelper.createRandomEventGivenSport(1, EventsTestHelper.FIRST_RANDOM_SPORT);
         Event secondEvent = EventsTestHelper.createRandomEventGivenSport(2, EventsTestHelper.SECOND_RANDOM_SPORT);
         Event thirdEvent = EventsTestHelper.createRandomEventGivenSport(3, EventsTestHelper.SECOND_RANDOM_SPORT);
@@ -169,17 +198,21 @@ public class EventDaoInMemoryTest {
         eventDao.create(firstEvent);
         eventDao.create(secondEvent);
 
+        // Act
         EventSearchCriteria eventSearchCriteria = new EventSearchCriteria();
         eventSearchCriteria.setDateStart(whithinRange.minusDays(1));
         eventSearchCriteria.setDateEnd(whithinRange.plusDays(1));
 
         List<Event> result = eventDao.search(eventSearchCriteria);
+
+        // Assert
         Assertions.assertThat(result.size()).isEqualTo(1);
         Assertions.assertThat(result.get(0).getId()).isEqualTo(secondEvent.getId());
     }
 
     @Test
     public void searchDateStartAndEndWhenDateIsSameDayAsEventThenReturnsFilteredResults() {
+        // Arrange
         Event firstEvent = EventsTestHelper.createRandomEventGivenSport(1, EventsTestHelper.FIRST_RANDOM_SPORT);
         Event secondEvent = EventsTestHelper.createRandomEventGivenSport(2, EventsTestHelper.SECOND_RANDOM_SPORT);
 
@@ -192,11 +225,14 @@ public class EventDaoInMemoryTest {
         eventDao.create(firstEvent);
         eventDao.create(secondEvent);
 
+        // Act
         EventSearchCriteria eventSearchCriteria = new EventSearchCriteria();
         eventSearchCriteria.setDateStart(dayThree);
         eventSearchCriteria.setDateEnd(dayThree);
 
         List<Event> result = eventDao.search(eventSearchCriteria);
+
+        // Assert
         Assertions.assertThat(result.size()).isEqualTo(1);
         Assertions.assertThat(result.get(0).getId()).isEqualTo(secondEvent.getId());
     }
