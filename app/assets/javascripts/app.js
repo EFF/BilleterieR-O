23db18e4831d68e93d0f67cb042bf8245c3dcf1b
Jacques-Olivier D. Bernier <jdesjardinsbernier@gmail.com>
2013-09-24 10:35:49 -0400

    handle callback error on front end

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,7 +3,7 @@ var controllers = require("./controllers");
 var app = angular.module('billetterieRO', []);
 
 app.config(['$routeProvider', '$locationProvider', function(routeProvider, locationProvider){
-    locationProvider.hashPrefix('!')
+    locationProvider.hashPrefix('!');
 
     routeProvider.when('/events', {controller: 'EventsController', templateUrl: '/assets/templates/events.html'});
     routeProvider.when('/events/:eventId', {controller: 'EventController', templateUrl: '/assets/templates/event.html'});
--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,4 +1,19 @@
 var events = function($scope, $http) {
+    $scope.events = null;
+
+    var apiCallSuccessCallback = function(result){
+        $scope.events = result
+    };
+
+    var apiCallErrorCallback = function(err){
+        console.error(err);
+        $scope.events = null;
+    };
+
+    $http.get('/api/events')
+        .success(apiCallSuccessCallback)
+        .error(apiCallErrorCallback);
+
     $http.get('/api/events').success(function(result){
         $scope.events = result;
     });
@@ -6,10 +21,20 @@ var events = function($scope, $http) {
 
 var event = function($scope, $http, $routeParams) {
     var eventId = $routeParams.eventId;
-    
-    $http.get('/api/events/' + eventId).success(function(result){
+    $scope.event = null;
+
+    var apiCallSuccessCallback = function(result){
         $scope.event = result
-    });
+    };
+
+    var apiCallErrorCallback = function(err){
+        console.error(err);
+        $scope.event = null;
+    };
+    
+    $http.get('/api/events/' + eventId)
+        .success(apiCallSuccessCallback)
+        .error(apiCallErrorCallback);
 }
 
 module.exports = {
