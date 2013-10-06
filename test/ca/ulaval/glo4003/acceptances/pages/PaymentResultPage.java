ea6f3582d42dea7ea12855cded899cf62c0ea1bd
b3nny1 <b3nny1@gmail.com>
2013-10-06 14:50:08 -0400

    added acceptation test for payment

---
+++ test/ca/ulaval/glo4003/acceptances/pages/PaymentResultPage.java
@@ -0,0 +1,11 @@
+package ca.ulaval.glo4003.acceptances.pages;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 06/10/13
+ * Time: 2:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PaymentResultPage {
+}
