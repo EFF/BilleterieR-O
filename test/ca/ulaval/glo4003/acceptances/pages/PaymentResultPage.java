61feb8c1cfa020f0e29236dd387e8c72b54bed2c
francois <frxpoulin@gmail.com>
2013-10-07 19:41:02 -0400

    add empty line

--- test/ca/ulaval/glo4003/acceptances/pages/PaymentResultPage.java
+++ test/ca/ulaval/glo4003/acceptances/pages/PaymentResultPage.java
@@ -3,6 +3,7 @@ package ca.ulaval.glo4003.acceptances.pages;
 import org.openqa.selenium.WebDriver;
 
 public class PaymentResultPage extends BaseFluentPage{
+
     public PaymentResultPage(WebDriver driver) {
         super(driver);
     }
