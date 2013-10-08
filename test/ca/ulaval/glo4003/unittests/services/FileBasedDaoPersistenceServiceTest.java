33730bc0c98ddc01ced3d70ecbd0d6a4fd008ada
Sylvain Perron <sylvain.perron@protorisk.com>
2013-10-08 01:52:56 -0400

    Added some more persistence tests

--- test/ca/ulaval/glo4003/unittests/services/FileBasedDaoPersistenceServiceTest.java
+++ test/ca/ulaval/glo4003/unittests/services/FileBasedDaoPersistenceServiceTest.java
@@ -1,6 +1,7 @@
 package ca.ulaval.glo4003.unittests.services;
 
 import ca.ulaval.glo4003.dataaccessobjects.PersistedDao;
+import ca.ulaval.glo4003.dataaccessobjects.RecordNotFoundException;
 import ca.ulaval.glo4003.services.FileBasedDaoPersistenceService;
 import ca.ulaval.glo4003.unittests.dataaccessobjects.PersistedDaoTest;
 import org.apache.commons.io.FileUtils;
@@ -58,6 +59,23 @@ public class FileBasedDaoPersistenceServiceTest {
         assertThat(getPersistenceFilesDirectory().exists()).isTrue();
     }
 
+    @Test
+    public void persistAndRestoreWorks() throws IOException, ClassNotFoundException, RecordNotFoundException {
+        // Arrange
+        PersistedDaoTest.TestRecord record = new PersistedDaoTest.TestRecord();
+        record.setValue(123);
+        record.setId(666);
+        this.dao.create(record);
+
+        // Act
+        PersistedDao<PersistedDaoTest.TestRecord> dao2 = new PersistedDao<PersistedDaoTest.TestRecord>(persistenceService) {};
+        this.persistenceService.restore(dao2);
+
+        // Assert
+        assertThat(dao2.count()).isEqualTo(1);
+        assertThat(dao2.read(record.getId()).getValue()).isEqualTo(record.getValue());
+    }
+
     private void deleteAllData() throws IOException {
         FileUtils.deleteDirectory(this.getPersistenceFilesDirectory());
     }
@@ -65,5 +83,4 @@ public class FileBasedDaoPersistenceServiceTest {
     private File getPersistenceFilesDirectory() {
         return persistenceService.getDaoPersistencePath(this.dao).getParentFile();
     }
-
 }
