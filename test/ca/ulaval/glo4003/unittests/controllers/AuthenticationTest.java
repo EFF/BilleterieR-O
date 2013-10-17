c453f37693bd12e5e18cf76a34e0443b28764e66
Jean-Philippe Poulin de Courval <jeep87c@gmail.com>
2013-10-17 19:12:53 -0400

    First working version of UnitTest on a controller. Mock the Context, Request and Body.

--- test/ca/ulaval/glo4003/unittests/controllers/AuthenticationTest.java
+++ test/ca/ulaval/glo4003/unittests/controllers/AuthenticationTest.java
@@ -5,36 +5,79 @@ import ca.ulaval.glo4003.dataaccessobjects.UserDao;
 import ca.ulaval.glo4003.exceptions.RecordNotFoundException;
 import ca.ulaval.glo4003.models.User;
 import com.google.inject.Inject;
+import org.codehaus.jackson.node.ObjectNode;
 import org.jukito.JukitoRunner;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import play.libs.Json;
+import play.mvc.Http;
+import play.test.FakeApplication;
+import play.test.Helpers;
 
-import static org.junit.Assert.assertEquals;
+import java.util.HashMap;
+
 import static org.mockito.Mockito.*;
+import static play.test.Helpers.start;
+import static play.test.Helpers.stop;
 
 @RunWith(JukitoRunner.class)
 public class AuthenticationTest {
     @Inject
     Authentication authentication;
 
-    UserDao mockedUserDao;
+    //UserDao mockedUserDao;
     String username = "username";
     String password = "password";
 
     @Before
-    public void setupMocks() throws RecordNotFoundException {
-        mockedUserDao = mock(UserDao.class);
+    public void setupMocks(UserDao mockedUserDao) throws RecordNotFoundException {
+        ObjectNode json = Json.newObject();
+        json.put("username", username);
+        json.put("password", password);
+
+        //mockedUserDao = mock(UserDao.class);
         User mockedUser = mock(User.class);
         when(mockedUser.getEmail()).thenReturn("email@test.com");
 
+        Http.RequestBody mockedBody = mock(Http.RequestBody.class);
+        when(mockedBody.asJson()).thenReturn(json);
+
+        Http.Request mockedRequest = mock(Http.Request.class);
+        when(mockedRequest.body()).thenReturn(mockedBody);
+
         when(mockedUserDao.findByEmailAndPassword(anyString(), anyString())).thenReturn(mockedUser);
+
+        Http.Context.current.set(new Http.Context((long) 1, mock(play.api.mvc.RequestHeader.class),
+                mockedRequest, new HashMap<String, String>(), new HashMap<String, String>(),
+                new HashMap<String, Object>()));
     }
 
     @Test
-    public void dummyTest() throws RecordNotFoundException {
+    public void loginTest(UserDao mockedUserDao) throws RecordNotFoundException {
+        FakeApplication app = Helpers.fakeApplication();
+
+        start(app);
+
+        ObjectNode json = Json.newObject();
+        json.put("username", username);
+        json.put("password", password);
+
+        //Result result = callAction(
+        //        ca.ulaval.glo4003.controllers.routes.ref.Authentication.login(),
+        //        fakeRequest().withSession("email", "email@test.com").withJsonBody(json)
+        //);
+        authentication.login();
+
+        stop(app);
+
+        verify(mockedUserDao).findByEmailAndPassword(anyString(), anyString());
+        //assertEquals(200, status(result));
+    }
+
+    @Test
+    public void dummyTest(UserDao mockedUserDao) throws RecordNotFoundException {
         authentication.test();
-        assertEquals(true, true);
 
         verify(mockedUserDao).findByEmailAndPassword(anyString(), anyString());
     }
