c2d8016baeb66671139ee679c30591e615925979
b3nny1 <b3nny1@gmail.com>
2013-11-06 12:44:45 -0500

    added teamDao and bootstrap

---
+++ app/ca/ulaval/glo4003/dataaccessobjects/TeamDao.java
@@ -0,0 +1,11 @@
+package ca.ulaval.glo4003.dataaccessobjects;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 06/11/13
+ * Time: 10:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TeamDao {
+}
