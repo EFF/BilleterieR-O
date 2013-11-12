2ab4972879ecaa0ce204c2b4472f0dd4436e1e14
Vincent Roseberry <roseberryv@gmail.com>
2013-11-12 18:11:27 -0500

    Simplify findByEmail method.

--- app/ca/ulaval/glo4003/dataaccessobjects/UserDao.java
+++ app/ca/ulaval/glo4003/dataaccessobjects/UserDao.java
@@ -3,12 +3,7 @@ package ca.ulaval.glo4003.dataaccessobjects;
 import ca.ulaval.glo4003.exceptions.RecordNotFoundException;
 import ca.ulaval.glo4003.models.User;
 import ca.ulaval.glo4003.services.DaoPersistenceService;
-import com.google.common.base.Optional;
-import com.google.common.base.Predicate;
-import com.google.common.collect.FluentIterable;
 import com.google.inject.Inject;
-
-import javax.annotation.Nullable;
 
 public class UserDao extends PersistedDao<User> implements DataAccessObject<User> {
 
@@ -19,17 +14,10 @@ public class UserDao extends PersistedDao<User> implements DataAccessObject<User
     }
 
     public User findByEmail(final String email) throws RecordNotFoundException {
-        FluentIterable<User> users = FluentIterable.from(this.list());
-
-        Optional<User> userOptional = users.firstMatch(new Predicate<User>() {
-            @Override
-            public boolean apply(@Nullable User user) {
-                return user.getEmail().toLowerCase().equals(email.toLowerCase());
+        for(User user: list()) {
+            if(user.getEmail().toLowerCase().equals(email.toLowerCase())) {
+                return user;
             }
-        });
-
-        if(userOptional.isPresent()) {
-            return userOptional.get();
         }
 
         throw new RecordNotFoundException();
