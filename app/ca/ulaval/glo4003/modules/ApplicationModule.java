00d356a81c546b0932b29c82c646caa4e0cb446e
Sylvain Perron <sylvain.perron@protorisk.com>
2013-11-13 14:30:38 -0500

    Fixed some running issue

--- app/ca/ulaval/glo4003/modules/ApplicationModule.java
+++ app/ca/ulaval/glo4003/modules/ApplicationModule.java
@@ -2,10 +2,9 @@ package ca.ulaval.glo4003.modules;
 
 import ca.ulaval.glo4003.dataaccessobjects.EventDao;
 import ca.ulaval.glo4003.dataaccessobjects.SportDao;
+import ca.ulaval.glo4003.dataaccessobjects.TransactionDao;
 import ca.ulaval.glo4003.dataaccessobjects.UserDao;
-import ca.ulaval.glo4003.services.CheckoutService;
-import ca.ulaval.glo4003.services.ConcreteCheckoutService;
-import ca.ulaval.glo4003.services.DaoPersistenceService;
+import ca.ulaval.glo4003.services.*;
 import com.google.inject.AbstractModule;
 
 public class ApplicationModule extends AbstractModule {
@@ -22,11 +21,15 @@ public class ApplicationModule extends AbstractModule {
         EventDao eventDao = new EventDao(this.persistenceService);
         SportDao sportDao = new SportDao(this.persistenceService);
         UserDao userDao = new UserDao(this.persistenceService);
+        TransactionDao transactionDao = new TransactionDao(this.persistenceService);
 
         bind(EventDao.class).toInstance(eventDao);
         bind(SportDao.class).toInstance(sportDao);
         bind(UserDao.class).toInstance(userDao);
+        bind(UserDao.class).toInstance(userDao);
+        bind(TransactionDao.class).toInstance(transactionDao);
 
+        bind(EmailService.class).to(ConcreteEmailService.class);
         bind(CheckoutService.class).to(ConcreteCheckoutService.class);
     }
 }
--- app/ca/ulaval/glo4003/services/ConcreteCheckoutService.java
+++ app/ca/ulaval/glo4003/services/ConcreteCheckoutService.java
@@ -4,12 +4,14 @@ import ca.ulaval.glo4003.ConstantsManager;
 import ca.ulaval.glo4003.dataaccessobjects.TransactionDao;
 import ca.ulaval.glo4003.models.Transaction;
 import ca.ulaval.glo4003.models.User;
+import com.google.inject.Inject;
 
 public class ConcreteCheckoutService implements CheckoutService {
 
     private TransactionDao transactionDao;
     private EmailService emailService;
 
+    @Inject
     public ConcreteCheckoutService(TransactionDao transactionDao, EmailService emailService) {
         this.transactionDao = transactionDao;
         this.emailService = emailService;
