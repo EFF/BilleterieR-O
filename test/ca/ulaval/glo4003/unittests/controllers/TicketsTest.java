655fd6357fd5c6a75586f6d0aaf2adcddb9171f8
b3nny1 <b3nny1@gmail.com>
2013-11-14 13:17:56 -0500

    temp commit

---
+++ test/ca/ulaval/glo4003/unittests/controllers/TicketsTest.java
@@ -0,0 +1,11 @@
+package ca.ulaval.glo4003.unittests.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 14/11/13
+ * Time: 9:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TicketsTest {
+}
