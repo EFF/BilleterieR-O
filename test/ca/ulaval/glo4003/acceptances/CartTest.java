a1efe087d707a4db0a4fda990981ba8777f76ef0
b3nny1 <b3nny1@gmail.com>
2013-11-22 21:35:39 -0500

    added a wait to fix test

--- test/ca/ulaval/glo4003/acceptances/CartTest.java
+++ test/ca/ulaval/glo4003/acceptances/CartTest.java
@@ -174,6 +174,7 @@ public class CartTest {
                 goToCartPage(cartPage, 1);
                 cartPage.payWithCreditCard();
                 cartPage.confirm(browser.getDriver());
+                cartPage.waitUnitlCartHasSize(0);
                 assertEquals(0, resultPage.getCartSize());
             }
         });
