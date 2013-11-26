385f5a6b2caadd023e15b3ce3fe740ab3b37c111
Jacques-Olivier D. Bernier <jdesjardinsbernier@gmail.com>
2013-11-26 17:13:49 -0500

    UserService

--- app/assets/javascripts/modules/user/user-controller.js
+++ app/assets/javascripts/modules/user/user-controller.js
@@ -1,6 +1,6 @@
 define(['./module'], function (UserModule) {
-    UserModule.controller('UserController', ['$scope', '$location', '$http', 'Login', 'FlashMessage',
-        function ($scope, $location, $http, Login, FlashMessage) {
+    UserModule.controller('UserController', ['$scope', '$location', '$http', 'Login', 'FlashMessage', 'UserService',
+        function ($scope, $location, $http, Login, FlashMessage, UserService) {
             if (Login.isLoggedIn) {
                 $scope.email = Login.username;
             } else {
@@ -10,29 +10,34 @@ define(['./module'], function (UserModule) {
             $scope.updatePassword = function () {
                 if ($scope.password != $scope.passwordConfirmation) {
                     FlashMessage.send("error", "Les deux mots de passe doivent être identiques.")
-                    return;
                 }
-                $http.post('/api/user/password', {
-                    actualPassword: $scope.actualPassword,
-                    password: $scope.password
-                }).success(function () {
-                        FlashMessage.send("success", "Votre mot de passe a été modifié avec succès.");
-                    }).error(function () {
-                        FlashMessage.send("error", "Erreur lors de la modification de votre mot de passe.");
-                    });
+                else {
+                    UserService.updatePassword($scope.actualPassword, $scope.password)
+                        .then(onUpdatePasswordSuccess, onUpdatePasswordError);
+                }
             };
 
             $scope.updateEmail = function () {
                 var email = $scope.email;
-                $http.post('api/user/email', {
-                    username: email
-                }).success(function () {
-                        Login.username = email;
-                        FlashMessage.send("success", "Votre email a été modifié avec succès.");
-                    }).error(function () {
-                        FlashMessage.send("error", "La modification de votre email a échouée");
-                    });
-            }
+                UserService.updateEmail(email).then(onUpdateEmailSuccess, onUpdateEmailError);
+            };
+
+            var onUpdatePasswordSuccess = function () {
+                FlashMessage.send("success", "Votre mot de passe a été modifié avec succès.");
+            };
+
+            var onUpdatePasswordError = function () {
+                FlashMessage.send("error", "Erreur lors de la modification de votre mot de passe.");
+            };
+
+            var onUpdateEmailSuccess = function () {
+                Login.username = email;
+                FlashMessage.send("success", "Votre email a été modifié avec succès.");
+            };
+
+            var onUpdateEmailError = function () {
+                FlashMessage.send("error", "La modification de votre email a échouée");
+            };
         }
     ]);
 });
