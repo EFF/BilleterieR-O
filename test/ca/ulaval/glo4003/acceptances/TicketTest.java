82eb30c961764b3159cbe1625d391f215841af54
Jean-Philippe Poulin de Courval <jeep87c@gmail.com>
2013-11-25 19:41:22 -0500

    Fixed acceptance tests in TicketTest.

--- test/ca/ulaval/glo4003/acceptances/TicketTest.java
+++ test/ca/ulaval/glo4003/acceptances/TicketTest.java
@@ -18,7 +18,7 @@ public class TicketTest  extends FluentTest {
     public void requiredInfosAreDisplayed() {
         running(testServer(3333, fakeApplication(new TestGlobal())), FIREFOX, new F.Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
-                TicketPage ticketPage = new TicketPage(browser.getDriver(), 1500, 2);
+                TicketPage ticketPage = new TicketPage(browser.getDriver(), 1450, 2);
                 ticketPage.go();
                 ticketPage.isAt();
 
@@ -32,7 +32,7 @@ public class TicketTest  extends FluentTest {
         running(testServer(3333, fakeApplication(new TestGlobal())), FIREFOX, new F.Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 EventPage eventPage = new EventPage(browser.getDriver(), 2);
-                TicketPage ticketPage = new TicketPage(browser.getDriver(), 1400, 2);
+                TicketPage ticketPage = new TicketPage(browser.getDriver(), 1445, 2);
                 ticketPage.go();
                 ticketPage.isAt();
 
