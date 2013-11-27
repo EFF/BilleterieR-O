db6d789d17a2278265600cf4154e5298cc682420
Jacques-Olivier D. Bernier <jdesjardinsbernier@gmail.com>
2013-11-26 20:21:36 -0500

    readd angular minified

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,7 @@
 require.config({
     baseUrl: '/assets/javascripts/',
     paths: {
-        angular: '//ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular',
+        angular: '//ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min',
         angularCookies: '//ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular-cookies.min'
     },
     shim: {
